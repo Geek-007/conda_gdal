88d68c42c62aec2d95ae6349e9e60e64c083d633
José Gómez-Dans <jgomezdans@gmail.com>
2014-08-18 14:45:25 +0100

    Put the GDAL relevant files in gdal/

---
+++ gdal/run_test.py
@@ -0,0 +1,27 @@
+import osgeo._gdal
+import osgeo._gdalconst
+import osgeo._ogr
+import osgeo._osr
+import osgeo
+import gdal
+import gdalconst
+import ogr
+import osr
+
+print "OGR Vector drivers"
+print "=================="
+cnt = ogr.GetDriverCount()
+for i in range(cnt):
+    print(ogr.GetDriver(i).GetName())
+
+print "GDAL Raster drivers"
+print "==================="
+cnt = gdal.GetDriverCount()
+for i in xrange(cnt):
+    print gdal.GetDriver(i).LongName
+
+print "Total number of vector drivers: %d" % ogr.GetDriverCount()
+print "Total number of vector drivers: %d" % gdal.GetDriverCount()
+
+
+import os1_hw
